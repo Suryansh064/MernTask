774d2c866c67ec37df2bc8e745ff008897bbb4a8
Suryansh Singh <144591803+Suryansh064@users.noreply.github.com>
2025-06-09 22:35:25 +0530

    Update Completed.jsx

--- Frontend/src/pages/Completed.jsx
+++ Frontend/src/pages/Completed.jsx
@@ -1,39 +1,4 @@
-// import React, { useEffect } from "react";
-// import Cards from "../components/Home/Cards";
-// import useActions from "../hooks/useActions.jsx";
 
-// const Completed =()=>{
-//     const {
-//     Data,
-//     fetchTasks,
-//     handleComplete,
-//     handleImportant,
-//     deleteTask,
-//     } = useActions("http://localhost:5001/api/v2/getCompleteTask");
-
-//     useEffect(() => {
-//     fetchTasks();
-//     }, []);
-
-    
-//     return(
-//             <div className="min-h-screen">
-//         <div className="flex justify-center items-center mb-4">
-//             <h1 className="text-6xl font-bold text-red-700  underline mb-4 text-center">Completed Tasks</h1>
-//         </div>
-//         <div >
-//             <Cards
-//                 home={"false"}
-//                 data={Data}
-//                 handleImportant={handleImportant}
-//                 handleComplete={handleComplete}
-//                 deleteTask={deleteTask}
-//             />
-//         </div>
-//     </div>
-//     )
-// }
-// export default Completed;
 import React, { useEffect } from "react";
 import Cards from "../components/Home/Cards";
 import useActions from "../hooks/useActions.jsx";
@@ -45,7 +10,7 @@ const Completed = () => {
         handleComplete,
         handleImportant,
         deleteTask,
-    } = useActions("http://localhost:5001/api/v2/getCompleteTask");
+    } = useActions("https://merntask-3b8t.onrender.com/api/v2/getCompleteTask");
 
     useEffect(() => {
         fetchTasks();
