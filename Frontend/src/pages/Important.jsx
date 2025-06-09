c01bcdad3b67b072b80d565b7e35bdbee5dd2ad0
Suryansh Singh <144591803+Suryansh064@users.noreply.github.com>
2025-06-09 22:36:43 +0530

    Update Important.jsx

--- Frontend/src/pages/Important.jsx
+++ Frontend/src/pages/Important.jsx
@@ -1,42 +1,3 @@
-// import React, { useEffect } from "react";
-// import Cards from "../components/Home/Cards";
-// import useActions from "../hooks/useActions.jsx";
-
-
-// const Important = () => {
-//     const 
-//     {
-//     Data,
-//     fetchTasks,
-//     handleComplete,
-//     handleImportant,
-//     deleteTask,
-//     } = useActions("http://localhost:5001/api/v2/getImpTask");
-
-//     useEffect(() => {
-//     fetchTasks();
-//     }, []);
-
-    
-//     return (
-//             <div className="min-h-screen">
-//             <div className="flex justify-center items-center mb-4">
-//                 <h1 className="text-6xl font-bold text-red-700  underline mb-4 text-center">Important Tasks</h1>
-//             </div>
-//                 <div >
-//                     <Cards
-//                         home={"false"}
-//                         data={Data}
-//                         handleImportant={handleImportant}
-//                         handleComplete={handleComplete}
-//                         deleteTask={deleteTask}
-//                     />
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default Important;
 import React, { useEffect } from "react";
 import Cards from "../components/Home/Cards";
 import useActions from "../hooks/useActions.jsx";
@@ -48,7 +9,7 @@ const Important = () => {
         handleComplete,
         handleImportant,
         deleteTask,
-    } = useActions("http://localhost:5001/api/v2/getImpTask");
+    } = useActions("https://merntask-3b8t.onrender.com/api/v2/getImpTask");
 
     useEffect(() => {
         fetchTasks();
