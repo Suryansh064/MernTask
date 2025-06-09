bfdba617479ca7306a9d0e6d37fc2cf585ec0701
Suryansh Singh <144591803+Suryansh064@users.noreply.github.com>
2025-06-09 22:37:11 +0530

    Update Incomplete.jsx

--- Frontend/src/pages/Incomplete.jsx
+++ Frontend/src/pages/Incomplete.jsx
@@ -1,40 +1,3 @@
-// import React, { useEffect } from "react";
-// import Cards from "../components/Home/Cards";
-// import useActions from "../hooks/useActions.jsx";
-
-// const Incomplete = () => {
-//     const {
-//     Data,
-//     fetchTasks,
-//     handleComplete,
-//     handleImportant,
-//     deleteTask,
-//     } = useActions("http://localhost:5001/api/v2/getInCompleteTask");
-
-//     useEffect(() => {
-//     fetchTasks();
-//     }, []);
-
-//     return (
-//         <div className="min-h-screen">
-//         <div className="flex justify-center items-center mb-4">
-//             <h1 className="text-6xl font-bold text-red-700  underline mb-4 text-center">InCompleted Tasks</h1>
-//         </div>
-//         <div >
-//             <Cards
-//                 home={"false"}
-//                 data={Data}
-//                 handleImportant={handleImportant}
-//                 handleComplete={handleComplete}
-//                 deleteTask={deleteTask}
-//             />
-//         </div>
-//     </div>
-    
-// );
-// };
-
-// export default Incomplete;
 
 import React, { useEffect } from "react";
 import Cards from "../components/Home/Cards";
@@ -47,7 +10,7 @@ const Incomplete = () => {
         handleComplete,
         handleImportant,
         deleteTask,
-    } = useActions("http://localhost:5001/api/v2/getInCompleteTask");
+    } = useActions("https://merntask-3b8t.onrender.com/api/v2/getInCompleteTask");
 
     useEffect(() => {
         fetchTasks();
